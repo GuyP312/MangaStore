7838c65c2d6298e02b5d3203e273885aa3e16b8d
GuyP <guyparnchinda@gmail.com>
2025-01-02 16:04:16 +0700

    create a model for manga using mongoose

---
+++ backend/models/mangaModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const mangaSchema = mongoose.Schema(
+    {
+        title:{
+            type: String,
+            required: true,
+        },
+        author:{
+            type: String,
+            required: true,
+        },
+        publishYear:{
+            type: Number,
+            required: true,
+        }
+    },
+    {
+        timestamps: true, //timestamps: true --> add 2 extra field : createdAt updateAt
+    }
+)
+
+export const Manga = mongoose.model('Manga',mangaSchema); //create a model call manga in DB
