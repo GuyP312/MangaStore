ce0dbe69dbf7d797eebb34bd9c930ab5a9bb6a5e
GuyP <guyparnchinda@gmail.com>
2025-01-05 21:39:27 +0700

    add card component

---
+++ frontend/src/components/home/MangaSingleCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { PiBookOpenTextLight } from 'react-icons/pi';
+import { BiUserCircle } from 'react-icons/bi';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from'react-icons/md';
+
+const MangaSingleCard = ({mangas}) => {
+  return (
+    <div
+    key = {mangas._id}
+    className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'>
+        <h2 className = 'absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg'>{mangas.publishYear}</h2>
+        <h4 className = 'my-2 text-gray-500'>{mangas._id}</h4>
+        <div className = 'flex justify-start items-center gap-x-2'>
+            <PiBookOpenTextLight className = 'text-red-300 text-2xl' />
+            <h2 className = 'my-1'>{mangas.title}</h2>
+        </div>
+        <div className = 'flex justify-start items-center gap-x-2'>
+            <BiUserCircle className = 'text-red-300 text-2xl'/>
+            <h2 className='my-1'>{mangas.author}</h2>
+        </div>
+        <div className = 'flex justify-between items-center gap-x-2 mt-4 p-4'>
+            <Link to={`/mangas/details/${mangas._id}`}>
+                <BsInfoCircle className='text-2xl text-green-800 hover:text-black' />
+            </Link>
+            <Link to={`/mangas/edit/${mangas._id}`}>
+                <AiOutlineEdit className='text-2xl text-yellow-600 hover:text-black' />
+            </Link>
+            <Link to={`/mangas/delete/${mangas._id}`}>
+                <MdOutlineDelete className='text-2xl text-red-600 hover:text-black'/>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default MangaSingleCard
