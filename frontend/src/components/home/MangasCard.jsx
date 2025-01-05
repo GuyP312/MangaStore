5ae56a46a5a2e2dc6082c255075c73c50891817f
GuyP <guyparnchinda@gmail.com>
2025-01-05 21:17:08 +0700

    add button to show either in card or table

---
+++ frontend/src/components/home/MangasCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MangasCard = () => {
+  return (
+    <div>MangasCard</div>
+  )
+}
+
+export default MangasCard
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,10 +5,13 @@ import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import MangasCard from '../components/home/MangasCard';
+import MangasTable from '../components/home/MangasTable';
 
 const Home = () => {
   const [mangas, setMangas] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [showType,setShowType] = useState('table');
   useEffect(() => {
     setLoading(true);
     axios.get('http://localhost:5555/mangas').then((res) => {
@@ -22,48 +25,19 @@ const Home = () => {
   }, []);
   return (
     <div className='p-4'>
+      <div className = 'flex justify-center items-center gap-x-4'>
+        <button className = 'bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg' onClick = {()=>setShowType('table')}>Table</button>
+        <button className = 'bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg' onClick = {()=>setShowType('card')}>Card</button>
+      </div>
       <div className='flex justify-between items-center'>
         <h1 className='text-3xl my-8'>Mangas List</h1>
         <Link to='/mangas/create'>
           <MdOutlineAddBox className='text-sky-800 text-4x1' />
         </Link>
       </div>
-      {loading ? (<Spinner />) : (
-        <table className='w-full border-separate border-spacing-2'>
-          <thead>
-            <tr>
-              <th className='border border-slate-600 rounded-md'>No</th>
-              <th className='border border-slate-600 rounded-md'>Title</th>
-              <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
-              <th className='border border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
-              <th className='border border-slate-600 rounded-md'>Operations</th>
-            </tr>
-          </thead>
-          <tbody>
-            {mangas.map((manga, index) => (
-              <tr key={manga._id} className='h-8'>
-                <td className='border border-slate-700 rounded-md text-center'>{index + 1}</td>
-                <td className='border border-slate-700 rounded-md text-center'>{manga.title}</td>
-                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>{manga.author}</td>
-                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>{manga.publishYear}</td>
-                <td className='border border-slate-700 rounded-md text-center'>
-                  <div className='flex justify-center gap-x-4'>
-                    <Link to={`/mangas/details/${manga._id}`}>
-                      <BsInfoCircle className='text-2xl text-green-800' />
-                    </Link>
-                    <Link to={`/mangas/edit/${manga._id}`}>
-                      <AiOutlineEdit className='text-2xl text-yellow-600' />
-                    </Link>
-                    <Link to={`/mangas/delete/${manga._id}`}>
-                      <MdOutlineDelete className='text-2xl text-red-600' />
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-
-        </table>)}
+      {loading ? (<Spinner />) : ( showType ==='table' ?
+        (<MangasTable mangas={mangas}/>) : (<MangasCard mangas = {mangas}/>) //HOLY TERNARY INSIDE A TERNARY
+      )}
     </div>
   )
 }
