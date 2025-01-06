f4d31715bd3846d6568ac0ea3c2c7a53ae74645d
GuyP <guyparnchinda@gmail.com>
2025-01-06 20:47:52 +0700

    Add star rating and description in createMangas

--- frontend/src/pages/CreateMangas.jsx
+++ frontend/src/pages/CreateMangas.jsx
@@ -9,6 +9,8 @@ const createMangas = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const [publishYear, setPublishYear] = useState('');
+  const [description, setDescription] = useState('');
+  const [rating, setRating] = useState('');
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const { enqueueSnackbar } = useSnackbar();
@@ -17,6 +19,8 @@ const createMangas = () => {
       title,
       author,
       publishYear,
+      description,
+      rating
     }
     setLoading(true);
     axios.post('http://localhost:5555/mangas', data) //sent post request to backend therefore update mongoDB
@@ -50,6 +54,14 @@ const createMangas = () => {
         <div className = 'my-4'>
           <label className = 'text-xl mr-4 text-gray-500'>Publish Year</label>
           <input type ='text' value = {publishYear} onChange={(e)=>setPublishYear(e.target.value)} className = 'border-2 border-gray-500 px-4 py-2 w-full' />
+        </div>
+        <div className = 'my-4'>
+          <label className = 'text-xl mr-4 text-gray-500'>Description</label>
+          <textarea value = {description} onChange={(e)=>setDescription(e.target.value)} className = 'border-2 border-gray-500 px-4 py-2 w-full' />
+        </div>
+        <div className = 'my-4'>
+          <label className = 'text-xl mr-4 text-gray-500'>Rating</label>
+          <input type ='number' min="0" max="5" step="1" value = {rating} onChange={(e)=>setRating(e.target.value)} className = 'border-2 border-gray-500 px-4 py-2 w-full' />
         </div>
         <button className = 'p-2 bg-sky-300 m-8' onClick = {handleSaveManga}>Save</button>
       </div>
